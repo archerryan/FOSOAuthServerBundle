e5a8b559424bc05400ddf0ee7e27986bbef24630
Aaron Opela <aaron.opela@netgalley.com>
2023-08-02 10:29:36 -0700

    NGP-6240 Resolve deprecation - AbstractType::buildForm() might add return type "void"

--- Form/Type/AuthorizeFormType.php
+++ Form/Type/AuthorizeFormType.php
@@ -23,7 +23,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class AuthorizeFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('client_id', HiddenType::class);
         $builder->add('response_type', HiddenType::class);
